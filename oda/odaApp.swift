2adb1a80e3e62779a35c099f7a08c9a0c1d90e5f
stinek4 <54266608+stinek4@users.noreply.github.com>
2021-10-14 13:22:37 +0200

    Initial Commit

---
+++ oda/odaApp.swift
@@ -0,0 +1,17 @@
+//
+//  odaApp.swift
+//  oda
+//
+//  Created by Stine Marie Vørner on 14/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct odaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
